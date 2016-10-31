730f85c9a9a7be35f3779973b6b239a2fcb7dbba
honingwon <honingwon@gmail.com>
2016-10-31 21:59:41 +0800

    modify GUI

--- renderer.js
+++ renderer.js
@@ -1,70 +1,29 @@
-/* require XLSX */
-var XLSX = require('XLSX')
+var saveXLSX = require('./xlsxsaver').saveXLSX;
 
 document.body.addEventListener('click', function (event) {
-  if (event.target.dataset.submit) {
-    submit(event)    
-  } 
+  if (event.target.dataset.button === 'ok') {
+    ok(event);
+  } else if(event.target.dataset.button === 'no') {
+  	no(event);
+  }
 })
 
-function submit (e) {
-  var form = document.querySelectorAll('form')[0];
-  console.log(form);
-  saveXLSX();
+function ok (e) {
+  if (confirm('确定选择满意?')) {
+  	console.log('yes');
+  } else {
+  	console.log('no');
+  }
   e.preventDefault();
 }
 
-function saveXLSX() {
-  /* original data */
-  var data = [[1,2,3],[true, false, null, "sheetjs"],["foo","bar",new Date("2014-02-19T14:30Z"), "0.3"], ["baz", null, "qux"]]
-  var ws_name = "SheetJS";
 
-  var wb = new Workbook(), ws = sheet_from_array_of_arrays(data);
-
-  /* add worksheet to workbook */
-  wb.SheetNames.push(ws_name);
-  wb.Sheets[ws_name] = ws;
-
-  /* write file */
-  XLSX.writeFile(wb, 'test.xlsx');
+function no (e) {
+  if (confirm('确定选择不满意?')) {
+  	console.log('yes');
+  } else {
+  	console.log('no');
+  }
+  e.preventDefault();
 }
 
-function datenum(v, date1904) {
-	if(date1904) v+=1462;
-	var epoch = Date.parse(v);
-	return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
-}
-
-function sheet_from_array_of_arrays(data, opts) {
-	var ws = {};
-	var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
-	for(var R = 0; R != data.length; ++R) {
-		for(var C = 0; C != data[R].length; ++C) {
-			if(range.s.r > R) range.s.r = R;
-			if(range.s.c > C) range.s.c = C;
-			if(range.e.r < R) range.e.r = R;
-			if(range.e.c < C) range.e.c = C;
-			var cell = {v: data[R][C] };
-			if(cell.v == null) continue;
-			var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
-			
-			if(typeof cell.v === 'number') cell.t = 'n';
-			else if(typeof cell.v === 'boolean') cell.t = 'b';
-			else if(cell.v instanceof Date) {
-				cell.t = 'n'; cell.z = XLSX.SSF._table[14];
-				cell.v = datenum(cell.v);
-			}
-			else cell.t = 's';
-			
-			ws[cell_ref] = cell;
-		}
-	}
-	if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
-	return ws;
-}
-
-function Workbook() {
-	if(!(this instanceof Workbook)) return new Workbook();
-	this.SheetNames = [];
-	this.Sheets = {};
-}
