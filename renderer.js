d3960a51160be5c0d86944f0826b8c41dd48f3ca
王鸿源honingwon <honingwon@gmail.com>
2016-11-02 10:14:00 +0800

    modify gui

--- renderer.js
+++ renderer.js
@@ -1,25 +1,65 @@
 var saveXLSX = require('./xlsxsaver').saveXLSX;
+var $ = require('./jquery');
+var staffset = $('#staffset');
+var wrongset = $('#wrongset');
 
 document.body.addEventListener('click', function (event) {
   if (event.target.dataset.button === 'ok') {
-    ok(event);
+    if(validate()) {
+      ok(event);
+    }
   } else if(event.target.dataset.button === 'no') {
-  	no(event);
+    if(validate()) {
+      no(event);
+    }
+  } else if(event.target.nodeName == 'INPUT' || event.target.nodeName == 'LABEL') {
+    updatePreview();
   }
+
+
 })
+
+function updatePreview (){
+  console.log('selected staff' , getstaff() ? getstaff().id : '未选择')
+  console.log('selected wrong' , getwrong() ? getwrong().id : '未选择')
+}
+
+function clearChecked (){
+  $('input:checked', staffset).prop('checked',false);
+  $('input:checked', wrongset).prop('checked',false);
+}
+
+function getstaff() {
+  return $('input:checked',staffset)[0];
+}
+
+function getwrong() {
+  return $('input:checked',wrongset)[0];
+}
+
+function validate() {
+  var ok = getstaff() !== undefined && getwrong() !== undefined;
+  if(!ok) {
+    alert('员工或错误类型未选择');
+  }
+  return ok;
+}
 
 function ok (e) {
   if (confirm('确定选择满意?')) {
+    clearChecked();
   	console.log('yes');
   } else {
   	console.log('no');
   }
   e.preventDefault();
+  
 }
 
 
 function no (e) {
   if (confirm('确定选择不满意?')) {
+    clearChecked();
   	console.log('yes');
   } else {
   	console.log('no');
